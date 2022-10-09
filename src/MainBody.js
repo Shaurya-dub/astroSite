1500155f635a8cffccbb7ba78df175be6141f3ea
Shaurya-dub <shauryadubey@hotmail.ca>
2022-10-09 14:58:03 -0400

    basic navbar styling done (for now)

---
+++ src/MainBody.js
@@ -0,0 +1,16 @@
+function MainBody() {
+  return (
+    <>
+      <section>
+        <div className="videoHolder">
+          <iframe
+            width="420"
+            height="315"
+            src="https://www.youtube.com/embed/tgbNymZ7vqY"
+          ></iframe>
+        </div>
+      </section>
+    </>
+  );
+}
+export default MainBody;
---
+++ src/NavBar.js
@@ -0,0 +1,23 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+function NavBar() {
+  return (
+    <Navbar variant='dark' className="navigation" expand="lg">
+      <Container className="navigationContainer">
+        <Navbar.Brand href="#home">Astro Site</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Book</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+          <button className="signIn">Sign-in</button>{" "}
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
