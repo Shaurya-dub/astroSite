584448239aead6959299eb2d5ad78ebb62d63ba9
Shaurya-dub <shauryadubey@hotmail.ca>
2022-10-25 12:32:29 -0400

    added switch components for navigation purposes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./NavBar";
 import MainBody from "./MainBody";
+import Services from "./Services"
 import Footer from "./Footer";
 
 function App() {
   
   return (
     <div>
-      <NavBar />
-      <MainBody />
-      <Footer />
+      <Router>
+        <NavBar />
+        <Switch path="/" component={MainBody}>
+        </Switch> 
+        <Switch path="/Services" component={Services}>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
