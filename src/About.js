49c0483cd063f349c05294a11c51b412672c3498
Shaurya-dub <shauryadubey@hotmail.ca>
2022-11-01 16:55:54 -0400

    created About component

---
+++ src/About.js
@@ -0,0 +1,30 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const About = () => {
+  return (
+    <Container>
+      <Row>
+        <div className="aboutHolder">
+          <div>
+            <img src="https://placekitten.com/250/250" alt="placeholder" />
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis optio sequi similique tenetur quod dolorum nemo fugiat minus consequatur ad quaerat, incidunt ex aliquid fugit veritatis obcaecati molestias, enim itaque?
+            Molestias minima, voluptates iure totam adipisci blanditiis tempora corporis commodi eos autem omnis amet enim quos placeat pariatur mollitia in nesciunt, quo facilis architecto soluta optio consectetur? Excepturi, sed architecto.
+          </p>
+        </div>        
+        
+        <div className="aboutHolder">
+          <div>
+            <img src="https://placekitten.com/250/250" alt="placeholder" />
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis optio sequi similique tenetur quod dolorum nemo fugiat minus consequatur ad quaerat, incidunt ex aliquid fugit veritatis obcaecati molestias, enim itaque?
+            Molestias minima, voluptates iure totam adipisci blanditiis tempora corporis commodi eos autem omnis amet enim quos placeat pariatur mollitia in nesciunt, quo facilis architecto soluta optio consectetur? Excepturi, sed architecto.
+          </p>
+        </div>
+      </Row>
+    </Container>
+  );
+};
+export default About;
